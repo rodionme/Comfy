4d4d2d18f7eae859721bd75527b3f861abb74b10
Rodion Mescheryakov <msrodion@gmail.com>
2017-10-03 00:59:58 +0300

    Template of Category component made dynamic

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -2,41 +2,41 @@
   section#page-category.page-container.category-page
     main.page-content
       section.category
-        h3.category__title Название категории
+        h3.category__title {{ category.title }}
 
         .category__articles.category-articles
           ul.category-articles__list
-            li.category-articles__item
-              a.category-articles__item-link(href="#") Статья один
-            li.category-articles__item
-              a.category-articles__item-link(href="#") Статья два
-            li.category-articles__item
-              a.category-articles__item-link(href="#") Статья с очень-преочень длинным названием, которое вряд ли поместится на одной строчке без ее переноса на другую строку
-            li.category-articles__item
-              a.category-articles__item-link(href="#") Статья четыре
-            li.category-articles__item
-              a.category-articles__item-link(href="#") Статья пять
-            li.category-articles__item
-              a.category-articles__item-link(href="#") Статья шесть
-            li.category-articles__item
-              a.category-articles__item-link(href="#") Статья семь
-            li.category-articles__item
-              a.category-articles__item-link(href="#") Статья восемь
-            li.category-articles__item
-              a.category-articles__item-link(href="#") Статья девять
-            li.category-articles__item
-              a.category-articles__item-link(href="#") Статья десять
-            li.category-articles__item
-              a.category-articles__item-link(href="#") Статья одиннадцать
+            li.category-articles__item(v-for="article of category.articles", :key="article.id")
+              router-link.category-articles__item-link(:to="{ name: 'article', params: { id: article.id }}") {{ article.title }}
 
 </template>
 
 
 <script>
+  const category = {
+    id: 1,
+    title: 'Название категории',
+    articles: [
+      { id: 1, title: 'Статья один' },
+      { id: 2, title: 'Статья два' },
+      { id: 3, title: 'Статья с очень-преочень длинным названием, которое вряд ли поместится на одной строчке без ее переноса на другую строку' },
+      { id: 4, title: 'Статья четыре' },
+      { id: 5, title: 'Статья пять' },
+      { id: 6, title: 'Статья шесть' },
+      { id: 7, title: 'Статья семь' },
+      { id: 8, title: 'Статья восемь' },
+      { id: 9, title: 'Статья девять' },
+      { id: 10, title: 'Статья десять' },
+      { id: 11, title: 'Статья одиннадцать' },
+    ]
+  };
+
   export default {
     name: 'Category',
     data () {
-      return {};
+      return {
+        category
+      };
     }
   };
 
